65fbc95b3375b1029abc2dff10902500a4f8d426
Richard Brett <rbrett@ap.org>
2014-03-30 19:25:26 -0400

    initial ui changes

--- Ga2oo_Json/src/com/ga2oo/palendar/About.java
+++ Ga2oo_Json/src/com/ga2oo/palendar/About.java
@@ -2,7 +2,6 @@ package com.ga2oo.palendar;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 
